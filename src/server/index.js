a91570b484d4d387066e8f40a78d02164609ae35
Alirio Angel <alirioangelarenas@gmail.com>
2019-03-28 13:28:56 -0400

    Chat Basico con Socket.IO Alpha

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,23 @@
 require("./config/_config");
 const app = require("../app");
-const http = require("http").Server(app);
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io").listen(server);
 
-app.listen(process.env.PORT, () => {
+io.emit("some event", { for: "everyone" });
+
+io.on("connection", socket => {
+    console.log(`un usuario se ha conectado`);
+
+    socket.on("chat message", function(msg) {
+        io.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Usuario desconectado");
+    });
+});
+
+server.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto ${process.env.PORT}`);
 });
